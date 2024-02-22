1d76219c4577c7995900bea29ed03e6103d33884
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-22 23:52:26 +0000

    1

--- main.py
+++ main.py
@@ -16,7 +16,7 @@
 ## The cards in the list have equal probability of being drawn.
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
-
+decks = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 ##################### Hints #####################
 
 #Hint 1: Go to this website and try out the Blackjack game: 
